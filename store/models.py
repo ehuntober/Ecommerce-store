f4d9fd8a603d8dd4d88576d76f3adcd3eb2f2b45
ehuntober <ehuntercrypto@gmail.com>
2023-03-12 10:31:12 +0000

    store app modelling done

--- store/models.py
+++ store/models.py
@@ -2,10 +2,18 @@ from django.db import models
 
 # Create your models here.
 
+class Promotion(models.Model):
+    description = models.CharField(max_length=225)
+    discount = models.FloatField()
+    
+    #product set
+
 class Collection(models.Model):
     title = models.CharField(max_length=255)
+    featured_product = models.ForeignKey('Product', on_delete=models.SET_NULL,
+                                         null=True,related_name='+')
 
-class Product(models,Model):
+class Product(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
     
@@ -13,10 +21,11 @@ class Product(models,Model):
     inventory = models.IntegerField()
     last_update = models.DateTimeField(auto_now=True)
     collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
+    promotions = models.ManyToManyField(Promotion,related_name='products')
     
     
 
-class Customer(modles.Model):
+class Customer(models.Model):
     
     MEMBERSHIP_BRONZE = 'B'
     MEMBERSHIP_SILVER = 'S'
@@ -51,23 +60,23 @@ class Order(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
     
 
-class Address(mdoels.Model):
+class Address(models.Model):
     street = models.CharField(max_length=255)
     city = models.CharField(max_length=255)
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE,primary_key=True)
     
 class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.PROTECT)
-    product = models.ForeignKey(Product, on_delete.models.PROTECT)
+    product = models.ForeignKey(Product, on_delete=models.PROTECT)
     quantity = models.PositiveSmallIntegerField()
     unit_pric = models.DecimalField(max_digits=6,decimal_places=2)
     
     
 class Cart(models.Model):
-    created_at = model.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
     
-class CartItem(models.Modle):
+class CartItem(models.Model):
     cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    quantity = models.PositiveISmallIntegerField()
+    quantity = models.PositiveSmallIntegerField()
     

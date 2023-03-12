205cc6c5aa70544d54ff10190ef9a59c15f72c25
ehuntober <ehuntercrypto@gmail.com>
2023-03-12 08:05:32 +0000

    order models created

--- store/models.py
+++ store/models.py
@@ -11,3 +11,36 @@ class Product(models,Model):
     last_update = models.DateTimeField(auto_now=True)
     
     
+
+class Customer(modles.Model):
+    
+    MEMBERSHIP_BRONZE = 'B'
+    MEMBERSHIP_SILVER = 'S'
+    MEMBERSHIP_GOLD = 'G'
+    
+    MEMBERSHIP_CHOICES = [
+        (MEMBERSHIP_BRONZE,'Bronze'),
+        (MEMBERSHIP_SILVER,'Silver'),
+        (MEMBERSHIP_GOLD,'Gold'),
+    ]
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=255)
+    birth_date = models.DateTimeField(null=True )
+    membership = models.CharField(max_length=1,choices=MEMBERSHIP_CHOICES,default=MEMBERSHIP_BRONZE)
+    
+
+class Order(models.Model):
+    PENDING = 'P'
+    COMPLETE = 'C'
+    FAILED = 'F'
+    
+    PAYMENT_STATUS = [
+        (PENDING,'Pending'),
+        (COMPLETE,'Complete'),
+        (FAILED, 'Failed')
+    ]
+    
+    placed_at = models.DateField(auto_now=True)
+    

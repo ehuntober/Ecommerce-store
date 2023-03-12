3fe5e3439f127054124e0838ed2e4c1d2cabe1d7
ehuntober <ehuntercrypto@gmail.com>
2023-03-12 08:35:01 +0000

    models modified

--- store/models.py
+++ store/models.py
@@ -32,15 +32,21 @@ class Customer(modles.Model):
     
 
 class Order(models.Model):
-    PENDING = 'P'
-    COMPLETE = 'C'
-    FAILED = 'F'
+    PAYMENT_STATUS_PENDING = 'P'
+    PAYMENT_STATUS_COMPLETE = 'C'
+    PAYMENT_STATUS_FAILED = 'F'
     
     PAYMENT_STATUS = [
-        (PENDING,'Pending'),
-        (COMPLETE,'Complete'),
-        (FAILED, 'Failed')
+        (PAYMENT_STATUS_PENDING,'Pending'),
+        (PAYMENT_STATUS_COMPLETE,'Complete'),
+        (PAYMENT_STATUS_FAILED, 'Failed')
     ]
     
-    placed_at = models.DateField(auto_now=True)
+    placed_at = models.DateField(auto_now_add=True)
+    payment_status = models.CharField(max_length=1, choices = PAYMENT_STATUS , default=PAYMENT_STATUS_PENDING)
     
+
+class Address(mdoels.Model):
+    street = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    customer = models.OneToOneField(Customer, on_delete=models.CASCADE,primary_key=True)

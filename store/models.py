f055fc67fa5de05d80c573ea7e1eb5b5d9c05d2a
ehuntober <ehuntercrypto@gmail.com>
2023-03-12 19:24:54 +0000

    new updated

--- store/models.py
+++ store/models.py
@@ -44,11 +44,7 @@ class Customer(models.Model):
     birth_date = models.DateTimeField(null=True )
     membership = models.CharField(max_length=1,choices=MEMBERSHIP_CHOICES,default=MEMBERSHIP_BRONZE)
     
-    class Meta:
-        db_table = 'store_customers'
-        indexes = [
-            models.Index(fields=['last_name', 'first_name'])
-        ]
+
     
 
 class Order(models.Model):

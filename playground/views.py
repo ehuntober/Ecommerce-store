f6837a56258e60fc4cc23e622c2ff18d0d0cdd32
ehuntober <ehuntercrypto@gmail.com>
2023-03-15 08:37:02 +0000

    no exception

--- playground/views.py
+++ playground/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 
 from django.http import HttpResponse
-from django.core.execpetion import ObjectDoesNotExist
+from django.core.exceptions import ObjectDoesNotExist
 from store.models import Product
 
 def homepage(request):
     try:
         product = Product.objects.get(pk=0)
     except ObjectDoesNotExist:
+        pass
         
     return render (request,'index.html', {'name': 'Mosh'})

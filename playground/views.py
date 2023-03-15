e81790b97acaa5794494010c048bef3b1b5015cf
ehuntober <ehuntercrypto@gmail.com>
2023-03-15 12:32:42 +0000

    annotating new value to the database on customers

--- playground/views.py
+++ playground/views.py
@@ -4,12 +4,14 @@ from django.http import HttpResponse
 from django.db.models import Q
 from django.db.models.aggregates import Count , Max, Min, Avg, Sum 
 from django.core.exceptions import ObjectDoesNotExist
-from store.models import Product , OrderItem
+from store.models import Product , OrderItem, Customer
+from django.db.models import Value
 
 def homepage(request):
+    queryset = Customer.objects.annotate(is_new=Value(True))
     # queryset = OrderItem.objects.values('product_id').distinct()
     # queryset = Product.objects.values_list('id','title','collection__title')
-    result = Product.objects.aggregate(count=Count('id'),min_price = Min('unit_price'))
+    # result = Product.objects.aggregate(count=Count('id'),min_price = Min('unit_price'))
 
         
     return render (request,'index.html', {'name': 'Mosh', 'result': result})

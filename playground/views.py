6d5d35eab676fd2c9f9771c2e4560c0e0f5ac609
ehuntober <ehuntercrypto@gmail.com>
2023-03-15 08:19:18 +0000

    exception error fixed on the playground view

--- playground/views.py
+++ playground/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 
 from django.http import HttpResponse
+from django.core.execpetion import ObjectDoesNotExist
 from store.models import Product
 
 def homepage(request):
     
-    query_set = Product.objects.all()
-    for product in query_set:
-        print(product)
+    product = Product.objects.get(pk=0)
+    # for product in query_set:
+    #     print(product)
         
     return render (request,'index.html', {'name': 'Mosh'})

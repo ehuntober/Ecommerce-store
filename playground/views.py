f2f5dfd28ee1b98d8e94549559f81379546ad004
ehuntober <ehuntercrypto@gmail.com>
2023-03-15 07:49:00 +0000

    query status

--- playground/views.py
+++ playground/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 
 from django.http import HttpResponse
-
+from store.models import Product
 
 def homepage(request):
-    x = calculate()
+    
+    query_set = Product.objects.all()
+    for product in query_set:
+        print(product)
+        
     return render (request,'index.html', {'name': 'Mosh'})

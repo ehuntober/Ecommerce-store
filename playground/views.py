476c90620012aaa44e430dfe0436297050c13d9c
ehuntober <ehuntercrypto@gmail.com>
2023-03-15 11:20:13 +0000

    new commt

--- playground/views.py
+++ playground/views.py
@@ -3,10 +3,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.db.models import Q
 from django.core.exceptions import ObjectDoesNotExist
-from store.models import Product
+from store.models import Product , OrderItem
 
 def homepage(request):
-    queryst = Product.objects.values_list('id','title','collection__title')
+    # queryset = OrderItem.objects.values('product_id').distinct()
+    # queryset = Product.objects.values_list('id','title','collection__title')
+    queryset = Product.objects.only('id','title')
 
         
     return render (request,'index.html', {'name': 'Mosh', 'products': list(queryset)})

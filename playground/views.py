07fa135dea4d22130140aae090176f32bc75458c
ehuntober <ehuntercrypto@gmail.com>
2023-03-15 13:20:24 +0000

    contating first and last name together

--- playground/views.py
+++ playground/views.py
@@ -5,13 +5,10 @@ from django.db.models import Q
 from django.db.models.aggregates import Count , Max, Min, Avg, Sum 
 from django.core.exceptions import ObjectDoesNotExist
 from store.models import Product , OrderItem, Customer
-from django.db.models import Value
+from django.db.models import Value , F
 
 def homepage(request):
-    queryset = Customer.objects.annotate(is_new=Value(True))
-    # queryset = OrderItem.objects.values('product_id').distinct()
-    # queryset = Product.objects.values_list('id','title','collection__title')
-    # result = Product.objects.aggregate(count=Count('id'),min_price = Min('unit_price'))
+    queryset = Customer.objects.annotate(new_id = F('id')+ 1)
 
         
-    return render (request,'index.html', {'name': 'Mosh', 'result': result})
+    return render (request,'index.html', {'name': 'Mosh', 'result': queryset})

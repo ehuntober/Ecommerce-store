b5f20185878cde47782b7e5cda6e199c8487ce59
ehuntober <ehuntercrypto@gmail.com>
2023-03-15 09:02:16 +0000

    complex filtering

--- playground/views.py
+++ playground/views.py
@@ -5,7 +5,7 @@ from django.core.exceptions import ObjectDoesNotExist
 from store.models import Product
 
 def homepage(request):
-    queryset = Product.objects.filter(title__icontains='coffee')
+    queryset = Product.objects.filter(inventory__lt=10,unity_price__lt=20)
 
         
     return render (request,'index.html', {'name': 'Mosh', 'products': list(queryset)})
